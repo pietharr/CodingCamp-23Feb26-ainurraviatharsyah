559b488f784b70b90abada17da41abc8ba951a7c
pietharr <raviatharsyah@gmail.com>
2026-02-28 03:01:05 +0700

    Mini Project

---
+++ js/script.js
@@ -0,0 +1,32 @@
+const greetingName = document.getElementById("greetingName")
+
+if (greetingName) {
+
+  // Bersihin <br> kalau user hapus semua
+  greetingName.addEventListener("input", function () {
+    if (
+      greetingName.innerHTML === "<br>" ||
+      greetingName.textContent.trim() === ""
+    ) {
+      greetingName.innerHTML = ""
+    }
+  })
+
+}
+
+function showPopup(name, email, message) {
+  const popup = document.getElementById('popup')
+  const popupContent = document.getElementById('popup-content')
+
+  popupContent.innerHTML = `
+    <strong>Name:</strong> ${name} <br>
+    <strong>Email:</strong> ${email} <br>
+    <strong>Message:</strong> ${message}
+  `
+
+  popup.classList.remove('hidden')
+
+  setTimeout(() => {
+    popup.classList.add('hidden')
+  }, 3000)
+}
